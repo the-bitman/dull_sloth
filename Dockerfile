78de44ee0f194235e550a2286bcd0a4b26c5275c
Mark Holdeman <the_bitman@hotmail.com>
2021-06-13 10:47:59 -0700

    use Faker not the weird wrapper

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python
 COPY . /home
-RUN pip install --no-cache-dir pandas && \
+RUN pip install --no-cache-dir pandas Faker && \
     mkdir /home/report /home/data && \
     chmod +x /home/hello.py
 VOLUME /home/report /home/data
--- hello.py
+++ hello.py
@@ -4,3 +4,12 @@ print('Nice to meet you', name)
 from pandas import read_csv
 mydf = read_csv('mycsv.csv')
 print(mydf.head())
+
+from faker import Faker
+fake = Faker()
+print(fake.name())
+print(fake.address())
+print(fake.text())
+
+for _ in range(10):
+  print(fake.name())
